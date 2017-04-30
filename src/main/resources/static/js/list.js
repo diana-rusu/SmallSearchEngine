60cb820ce13dae73272e23f5bf52cce3d75e7e8d
Diana Rusu <diana.russu.87@gmail.com>
2017-04-30 18:20:37 +0200

    Add search function for elements in list(by title)

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -2,7 +2,22 @@ app.controller('listCtrl',['$scope','$state','$http','texts','textService', func
 
 $scope.texts = texts.data;
 
-console.log(texts.data);
+$scope.searchText=null;
+$scope.filterTitle = function(){
+var url=null;
+if($scope.searchText.length==0){
+url="/sort";
+}
+else{
+url='/name/'+$scope.searchText;
+}
+$http.get(url).then(function(data){
+
+$scope.texts = data.data;
+},function(data){
+alert("we have an error here");
+});
+};
 
 $scope.edit= function(text){
 textService.setText(text);
